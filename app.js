281cf49cf1f8a6749761ccbb95a878e2c76f0221
Ham9911 <mhammad8633@gmail.com>
2021-11-24 21:00:50 +0500

    a

---
+++ app.js
@@ -0,0 +1,23 @@
+let initValue=0;
+let counterVal=document.getElementById('counter');
+const increment=()=>{
+    initValue=initValue+1;
+    counterVal.innerHTML=initValue;
+}
+
+const decrement=()=>{
+    initValue=initValue-1;
+    counterVal.innerHTML=initValue;
+}
+
+const getVal=()=>{
+    const snap=document.getElementById('currVal');                
+    snap.innerHTML=initValue;
+    var span = document.createElement('div'); 
+    snap.appendChild(span)        
+    span.innerHTML=initValue;                            
+       
+}
+
+
+
